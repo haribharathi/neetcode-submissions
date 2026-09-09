d749128cef28c14fa82b2865c583b44cb013cfdc
haribharathi <36889562+haribharathi@users.noreply.github.com>
2026-09-09 22:09:05 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> output = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        traverse(new ArrayList<>(), nums, 0);
+        return output;
+    }
+
+    public void traverse(List<Integer> each, int[] nums, int index) {
+        if (index == nums.length) {
+            //System.out.println(each + " " + index);
+            output.add(each.stream().map(e-> e).toList());
+            return;
+        }
+        each.add(nums[index]);
+        //System.out.println(each + " " + index);
+        traverse(each, nums, index + 1);
+        each.remove(each.size() - 1);
+        //System.out.println(each + " " + index);
+        traverse(each, nums, index + 1);
+    }
+}
